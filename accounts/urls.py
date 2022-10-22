edfef487bc35815493b423f2392ded8d78743606
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-10-22 06:50:44 -0700

    Added registration page

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.SignUpView.as_view(), name='register'),
+]
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notebook.apps.NotebookConfig'
+    'notebook.apps.NotebookConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
