71f64c2ac60c463f3278dc434896c2e8df4aa180
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-10-22 07:21:25 -0700

    Styled login and registration pages

--- static/js/registration.js
+++ static/js/registration.js
@@ -24,10 +24,19 @@ setTheme(nord)
 // set placeholders on registration/login forms
 const usernameField = document.getElementById('id_username');
 const passwordField = document.getElementById('id_password');
-const password1Field = document.getElementById('id_password1');
-const password2Field = document.getElementById('id_password2');
-usernameField.placeholder = 'Username';
-passwordField.placeholder = 'Password';
-password1Field.placeholder = 'Password';
-password2Field.placeholder = 'Re-enter Password';
+const password1Field = document.querySelector('input#id_password1');
+const password2Field = document.querySelector('input#id_password2');
+
+if (usernameField) {
+    usernameField.placeholder = 'Username';
+}
+
+if (passwordField) {
+    passwordField.placeholder = 'Password';
+}
+
+if (password1Field && password2Field) {
+    password1Field.placeholder = 'Password';
+    password2Field.placeholder = 'Re-enter Password';
+}
 
